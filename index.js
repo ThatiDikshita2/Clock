9ee293fbdfd71a4c6eb76690403739d3d02b16a1
ThatiDikshita2 <150443832+ThatiDikshita2@users.noreply.github.com>
2024-10-25 12:11:34 +0530

    Update index.js
    Signed-off-by: ThatiDikshita2 <150443832+ThatiDikshita2@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const hourE1 = document.querySelector(".hour");
-const minuteE1 = document.querySelector(".minute");
-const secondE1 = document.querySelector(".second");
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
 
-function updateClock(){
-    const currentDate = new Date();
-   // setTimeout(updateClock, 1000);
-   const hour = currentDate.getHours();
-   const mintue = currentDate.getMintues();
-   const second = currentDate.getSeconds();
-   const hourDeg = (hour / 12) * 360;
-    hourE1.style.transform =  'rotate(${hourDeg}deg)';
-    const mintueDeg = (mintue / 60) * 360;
-    mintueE1.style.transform = 'rotate(${mintueDeg}deg)';
-    const secondDeg = (second / 60) * 360;
-    secondE1.style.transform = 'rotate(${secondDeg}deg)';
+function setDate() {
+  const now = new Date();
+
+  const getSecond = now.getSeconds();
+  const getMinute = now.getMinutes();
+  const getHour = now.getHours();
+
+  const secondDegree = (getSecond / 60) * 360;
+  const minuteDegree = (getMinute / 60) * 360;
+  const hourDegree = (getHour / 12) * 360;
+
+  second.style.transform = `rotate(${secondDegree}deg)`;
+  minute.style.transform = `rotate(${minuteDegree}deg)`;
+  hour.style.transform = `rotate(${hourDegree}deg)`;
 }
-  
-//updateClock();
 
-setInterval(updateClock, 1000)
-
+setInterval(setDate, 1000);
